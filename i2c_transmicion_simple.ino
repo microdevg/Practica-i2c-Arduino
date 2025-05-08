b585cee96fd3381d2976fe79da59c6f5b88fb515
German <germanvelardez16@gmail.com>
2025-05-08 16:09:39 -0300

    [LIbreria compatible con ESP32]

--- i2c_transmicion_simple.ino
+++ i2c_transmicion_simple.ino
@@ -1,19 +1,25 @@
 
 
 // PINES POR DEFECTO SDA 21, SCL 22
-
+#include <string.h>
 #include <LiquidCrystal_I2C.h>
 #include  <Wire.h>
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 
+char buffer[40]={0};
+int contador = 0;
+
 void setup() {
-  lcd.init();
+  lcd.begin();
   lcd.backlight();
+  lcd.clear();
 }
+
 void loop() {
+  sprintf(buffer,"Contador:%d",contador++);
   delay(1000);
   lcd.setCursor(0,0);
-  lcd.print("Hello, From");
-  lcd.setCursor(0,1);
-  lcd.print("ESP32 I2C LCD1602");
+  lcd.print(buffer);
+  Serial.print(buffer);
+
 }
